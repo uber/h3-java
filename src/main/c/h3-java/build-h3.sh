e9fc53122ca36f81e534ec7302e8bb452d0f0a43
Isaac Brodsky <isaac@uber.com>
2018-04-15 20:00:56 -0700

    Cross compile via dockcross
    Build for Android, Linux, and Windows via dockcross. Mac OSX is built only when it is the build platform. Change H3CoreLoader to detect Windows, Linux, etc. Its detection of architecture names is not complete.
    
    Add OSX to Travis CI.

--- src/main/c/h3-java/build-h3.sh
+++ src/main/c/h3-java/build-h3.sh
@@ -26,7 +26,7 @@
 # This script expects to be run from the project's base directory (where
 # pom.xml is) as part of the Maven build process.
 
-set -e
+set -ex
 
 GIT_REMOTE=$1
 GIT_REVISION=$2
@@ -51,22 +51,47 @@ H3_ROOT=`pwd`
 popd
 
 #
-# Now that H3 is downloaded, build H3-Java's native library and H3 along with
-# it.
+# Now that H3 is downloaded, build H3-Java's native library for this platform.
 #
 
 mkdir -p h3-java-build
 pushd h3-java-build
 
-cmake -DBUILD_SHARED_LIBS=ON "-DH3_ROOT=$H3_ROOT" -DCMAKE_BUILD_TYPE=Release ../../src/main/c/h3-java
+cmake -DUSE_NATIVE_JNI=ON -DBUILD_SHARED_LIBS=ON "-DH3_ROOT=$H3_ROOT" -DCMAKE_BUILD_TYPE=Release ../../src/main/c/h3-java
 make h3-java binding-functions
 
 popd
 popd
 
+# Copy the built artifact for this platform.
+if [ "$(uname -sm)" = "Darwin x86_64" ]; then
+    mkdir -p src/main/resources/darwin-x64
+    cp target/h3-java-build/lib/libh3-java.dylib src/main/resources/darwin-x64
+else
+    cp target/h3-java-build/lib/libh3-java* src/main/resources/
+fi
+
 #
-# Copy the built artifact into the source tree so it can be included in the
-# built JAR.
+# Now that H3 is downloaded, build H3-Java's native library for other platforms.
 #
 
-cp target/h3-java-build/lib/libh3* src/main/resources/
+# linux-armv6 excluded because of build failure
+for image in android-arm android-arm64 linux-arm64 linux-armv5 linux-armv7 linux-mipsel linux-mips linux-s390x linux-ppc64le linux-x64 linux-x86 windows-x64 windows-x86; do
+    # Setup for using dockcross
+    BUILD_ROOT=target/h3-java-build-$image
+    mkdir -p $BUILD_ROOT
+    docker pull dockcross/$image
+    docker run --rm dockcross/$image > $BUILD_ROOT/dockcross
+    chmod +x $BUILD_ROOT/dockcross
+
+    # Perform the actual build inside Docker
+    $BUILD_ROOT/dockcross --args "-v $JAVA_HOME:/java" bash -c "cd $BUILD_ROOT && cmake -DBUILD_SHARED_LIBS=ON -DH3_ROOT=/work/target/h3 -DCMAKE_BUILD_TYPE=Release /work/src/main/c/h3-java && make h3-java"
+
+    # Copy the built artifact into the source tree so it can be included in the
+    # built JAR.
+    OUTPUT_ROOT=src/main/resources/$image
+    mkdir -p $OUTPUT_ROOT
+    if [ -e $BUILD_ROOT/lib/libh3-java.so ]; then cp $BUILD_ROOT/lib/libh3-java.so $OUTPUT_ROOT ; fi
+    if [ -e $BUILD_ROOT/lib/libh3-java.dylib ]; then cp $BUILD_ROOT/lib/libh3-java.dylib $OUTPUT_ROOT ; fi
+    if [ -e $BUILD_ROOT/lib/libh3-java.dll ]; then cp $BUILD_ROOT/lib/libh3-java.dll $OUTPUT_ROOT ; fi
+done
